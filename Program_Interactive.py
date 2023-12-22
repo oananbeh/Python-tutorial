5c91d58552efc32bf5ac1420578bcd467b926138
Obieda Ananbeh <oananbeh@outlook.com>
2023-12-22 09:34:53 -0500

    Program _nteractive

---
+++ Program_Interactive.py
@@ -0,0 +1,24 @@
+
+# name = input("What's your name? ")
+# print("Hello, " + name + "! Welcome to the interactive world.")
+
+
+# name = input("What's your name? ")  # Get the user's name first
+# age = input("Hi %s, what about your age: "%(name))  # Now you can use name in the string
+
+# name = input("What's your name? ")  # Get the user's name first
+# age = input("Hi {}, what about your age: ".format(name)) 
+
+# print("""
+# Hello World.
+# My name is Obieda and
+# I am 34 years old.
+# """)
+
+# print(''''
+# Hello World.
+# My name is Obieda and
+# I am 34 years old.
+# ''')
+
+print('Hello \tWorld') # the output will be Hello World, with a tab space between 'Hello' and 'World'.
